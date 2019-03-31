5082a80bd7030cb6e50daab49386aa8733065f5a
Nicklas Nielsen <nnicklas92@gmail.com>
2019-03-31 16:33:39 +0200

    Data extraction completed

--- AR Wizard/Assets/LeapMotion/Core/Scripts/Hands/CapsuleHand.cs
+++ AR Wizard/Assets/LeapMotion/Core/Scripts/Hands/CapsuleHand.cs
@@ -7,6 +7,7 @@
  * between Leap Motion and you, your company or other organization.           *
  ******************************************************************************/
 
+using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -61,7 +62,15 @@ namespace Leap.Unity {
     private Hand _hand;
     private Vector3[] _spherePositions;
 
-    public override ModelType HandModelType {
+    private Dictionary<int, Mesh> _meshMap = new Dictionary<int, Mesh>();
+    private int _counterFrameRate = 0;
+    private int _counterWriteToCsv = 0;
+    private const int GESTURE_ITERATION = 30;
+    private readonly List<string> _dataDescription = new List<string>();
+    private readonly List<float> _data = new List<float>();
+    private static bool _onlyOnce;
+
+        public override ModelType HandModelType {
       get {
         return ModelType.Graphics;
       }
@@ -90,6 +99,7 @@ namespace Leap.Unity {
       if (_material != null) {
         _sphereMat = new Material(_material);
         _sphereMat.hideFlags = HideFlags.DontSaveInEditor;
+        InitCsv(_hand);
       }
     }
 
@@ -107,6 +117,46 @@ namespace Leap.Unity {
         _sphereMat.color = _rightColorList[_rightColorIndex];
         _rightColorIndex = (_rightColorIndex + 1) % _rightColorList.Length;
       }
+    }
+
+    private void InitCsv(Hand hand)
+    {
+        if (_onlyOnce) return;
+        _onlyOnce = true;
+
+        Debug.Log("Writing Description to file");
+        for (int i = 0; i < GESTURE_ITERATION; i++)
+        {
+            _dataDescription.Add($"{i}. Palm Position X");
+            _dataDescription.Add($"{i}. Palm Position Y");
+            _dataDescription.Add($"{i}. Palm Position Z");
+            _dataDescription.Add($"{i}. Palm Position Yaw");
+            _dataDescription.Add($"{i}. Palm Position Pitch");
+            _dataDescription.Add($"{i}. Palm Position Roll");
+            _dataDescription.Add($"{i}. Wrist Position X");
+            _dataDescription.Add($"{i}. Wrist Position Y");
+            _dataDescription.Add($"{i}. Wrist Position Z");
+            _dataDescription.Add($"{i}. Wrist Position Yaw");
+            _dataDescription.Add($"{i}. Wrist Position Pitch");
+            _dataDescription.Add($"{i}. Wrist Position Roll");
+
+            foreach (var handFinger in hand.Fingers)
+            {
+                foreach (var bone in handFinger.bones)
+                {
+                    _dataDescription.Add($"{i}. Finger: {handFinger.Id} Bone: {bone.Type} X");
+                    _dataDescription.Add($"{i}. Finger: {handFinger.Id} Bone: {bone.Type} Y");
+                    _dataDescription.Add($"{i}. Finger: {handFinger.Id} Bone: {bone.Type} Z");
+                    _dataDescription.Add($"{i}. Finger: {handFinger.Id} Bone: {bone.Type} Yaw");
+                    _dataDescription.Add($"{i}. Finger: {handFinger.Id} Bone: {bone.Type} Pitch");
+                    _dataDescription.Add($"{i}. Finger: {handFinger.Id} Bone: {bone.Type} Roll");
+                    _dataDescription.Add($"{i}. Palm Dist To Finger: {handFinger.Id} Bone: {bone.Type}");
+                }
+            }
+            }
+
+        File.AppendAllLines("HandGestureData.csv", new [] {string.Join(",", _dataDescription) });
+
     }
 
     public override void UpdateHand() {
@@ -196,51 +246,58 @@ namespace Leap.Unity {
       drawCylinder(mockThumbJointPos, THUMB_BASE_INDEX);
       drawCylinder(mockThumbJointPos, PINKY_BASE_INDEX);
 
-      if (counter++ >= 5)
+      if (_counterFrameRate++ >= 5)
       {
-          WriteOutData(_hand);
-          counter = 0;
+          ExtractHandData(_hand);
+          _counterFrameRate = 0;
+          if (++_counterWriteToCsv >= GESTURE_ITERATION)
+          {
+              WriteDataToCsv();
+              _data.RemoveRange(0, _dataDescription.Count/3);
+              _counterWriteToCsv = 20;
+          }
       }
     }
 
-    private void WriteOutData(Hand hand)
+    private void WriteDataToCsv()
     {
-        List<Data> data = new List<Data>();
+        Debug.Log("Writing Data to file");
+        File.AppendAllLines("HandGestureData.csv", new [] { string.Join(",", _data) });
+    }
+
+    private void ExtractHandData(Hand hand)
+    {
         //Palm
-        data.Add(new Data { Description = "Palm Position X", data = hand.PalmPosition.x});
-        data.Add(new Data { Description = "Palm Position Y", data = hand.PalmPosition.y });
-        data.Add(new Data { Description = "Palm Position Z", data = hand.PalmPosition.z });
-        data.Add(new Data { Description = "Palm Position Yaw", data = hand.PalmPosition.Yaw });
-        data.Add(new Data { Description = "Palm Position Pitch", data = hand.PalmPosition.Pitch });
-        data.Add(new Data { Description = "Palm Position Roll", data = hand.PalmPosition.Roll });
-        data.Add(new Data { Description = "Palm Position Sphere radius", data =  _palmRadius * transform.lossyScale.x});
+        _data.Add(hand.PalmPosition.x);
+        _data.Add(hand.PalmPosition.y);
+        _data.Add(hand.PalmPosition.z);
+        _data.Add(hand.PalmPosition.Yaw);
+        _data.Add(hand.PalmPosition.Pitch);
+        _data.Add(hand.PalmPosition.Roll);
 
         //Wrist
-        data.Add(new Data { Description = "Wrist Position X", data = hand.WristPosition.x });
-        data.Add(new Data { Description = "Wrist Position Y", data = hand.WristPosition.y });
-        data.Add(new Data { Description = "Wrist Position Z", data = hand.WristPosition.z });
-        data.Add(new Data { Description = "Wrist Position Yaw", data = hand.WristPosition.Yaw });
-        data.Add(new Data { Description = "Wrist Position Pitch", data = hand.WristPosition.Pitch });
-        data.Add(new Data { Description = "Wrist Position Roll", data = hand.WristPosition.Roll });
+        _data.Add(hand.WristPosition.x);
+        _data.Add(hand.WristPosition.y);
+        _data.Add(hand.WristPosition.z);
+        _data.Add(hand.WristPosition.Yaw);
+        _data.Add(hand.WristPosition.Pitch);
+        _data.Add(hand.WristPosition.Roll);
+
+
 
         //Fingers
         foreach (var handFinger in hand.Fingers)
         {
             foreach (var bone in handFinger.bones)
             {
-                data.Add(new Data { Description = $"Finger: {handFinger.Id}, Bone: {bone.Type}, X", data = bone.NextJoint.x });
-                data.Add(new Data { Description = $"Finger: {handFinger.Id}, Bone: {bone.Type}, Y", data = bone.NextJoint.y });
-                data.Add(new Data { Description = $"Finger: {handFinger.Id}, Bone: {bone.Type}, Z", data = bone.NextJoint.z });
-                data.Add(new Data { Description = $"Finger: {handFinger.Id}, Bone: {bone.Type}, Yaw", data = bone.NextJoint.Yaw });
-                data.Add(new Data { Description = $"Finger: {handFinger.Id}, Bone: {bone.Type}, Pitch", data = bone.NextJoint.Pitch });
-                data.Add(new Data { Description = $"Finger: {handFinger.Id}, Bone: {bone.Type}, Roll", data = bone.NextJoint.Roll });
-                data.Add(new Data { Description = $"Palm Dist To Finger: {handFinger.Id}, Bone: {bone.Type}", data = hand.PalmPosition.DistanceTo(bone.NextJoint)});
+                _data.Add(bone.NextJoint.x);
+                _data.Add(bone.NextJoint.y);
+                _data.Add(bone.NextJoint.z);
+                _data.Add(bone.NextJoint.Yaw);
+                _data.Add(bone.NextJoint.Pitch);
+                _data.Add(bone.NextJoint.Roll);
+                _data.Add(hand.PalmPosition.DistanceTo(bone.NextJoint));
             }
-        }
-
-        foreach (var d in data)
-        {
-            File.AppendAllLines("some path", new[] { $"{d.data}" });
         }
     }
 
@@ -283,9 +340,6 @@ namespace Leap.Unity {
     private int getFingerJointIndex(int fingerIndex, int jointIndex) {
       return fingerIndex * 4 + jointIndex;
     }
-
-    private Dictionary<int, Mesh> _meshMap = new Dictionary<int, Mesh>();
-    private int counter = 0;
 
     private Mesh getCylinderMesh(float length) {
       int lengthKey = Mathf.RoundToInt(length * 100 / CYLINDER_MESH_RESOLUTION);
